61db20bfc6950ce7d5df65f2b62ee9069041f397
crazyhaller <suvigyamadrid@gmail.com>
2023-09-17 19:58:32 +0530

    reactrouter in index & bootstraprouter in header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../../assets/logo.png'
 import './header.css'
 
@@ -8,21 +9,27 @@ const Header = () => {
     <header>
       <Navbar className="navbar-bg" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="ShopWave" />
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} alt="ShopWave" />
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle
             aria-controls="basic-navbar-nav"
             className="navbar-icon"
           />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart" className="text-white">
-                <FaShoppingCart className="navbar-icon" /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login" className="text-white">
-                <FaUser className="navbar-icon" /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link className="text-white">
+                  <FaShoppingCart className="navbar-icon" /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link className="text-white">
+                  <FaUser className="navbar-icon" /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Home from './Pages/Home/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
 

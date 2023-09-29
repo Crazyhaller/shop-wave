bc94e7da3179fd8427deccf8f522522bfd00c648
crazyhaller <suvigyamadrid@gmail.com>
2023-09-29 22:07:01 +0530

    setup paypal and order paid logic

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,14 +65,32 @@ const getOrderById = asyncHandler(async (req, res) => {
 })
 
 // @desc   Update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('Order paid')
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id, // from paypal
+      status: req.body.status, // from paypal
+      update_time: req.body.update_time, // from paypal
+      email_address: req.body.payer.email_address, // from paypal
+    }
+
+    const updatedOrder = await order.save()
+
+    res.status(200).json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
 })
 
 // @desc Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('Order delivered')
